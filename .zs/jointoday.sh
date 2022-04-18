13b513024825a5c29a417dcbd9144a1db6648204
James Mills <prologic@shortcircuit.net.au>
2022-04-18 15:26:02 +1000

    Avoid picking a down pod for Join Today CTA

--- .zs/jointoday.sh
+++ .zs/jointoday.sh
@@ -2,6 +2,14 @@
 
 pods="twtxt.net twt.nfld.uk we.loveprivacy.club yarn.yarnpods.com yarn.zn80.net neotxt.dk"
 
-random_pod="$(echo "$pods" | tr ' ' '\n' | shuf | head -n 1)"
+pick_random_pod() {
+  random_pod="$(echo "$pods" | tr ' ' '\n' | shuf | head -n 1)"
+  while ! curl -qsSL "https://${random_pod}/register" > /dev/null 2>&1; do
+    sleep 1
+    random_pod="$(echo "$pods" | tr ' ' '\n' | shuf | head -n 1)"
+  done
+  printf "%s" "$random_pod"
+}
 
-printf "<a class=\"button\" href=\"https://%s/register\">Join Today!</a>" "$random_pod"
+
+printf "<a class=\"button\" href=\"https://%s/register\">Join Today!</a>" "$(pick_random_pod)"
