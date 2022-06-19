28ad9061ec64d22a02331ef1774df24e49deab3e
James Mills <prologic@shortcircuit.net.au>
2022-06-20 09:36:50 +1000

    Remove Yarn Pods (broken?)

--- .zs/pods.sh
+++ .zs/pods.sh
@@ -12,7 +12,6 @@ cat <<EOF
 | [twtxt.net](https://twtxt.net)                      | Australia/Brisbane | [@prologic](https://twtxt.net/~prologic)               | $(get_pod_mau "https://twtxt.net")                |
 | [nfld](https://twt.nfld.uk)                         | United Kingdom     | [@jlj](https://twt.nfld.uk/~jlj)                       | $(get_pod_mau "https://twt.nfld.uk")              |
 | [We.Love.Privacy.Club](https://we.loveprivacy.club) | Australia          | [@eldersnake](https://we.loveprivacy.club/~eldersnake) | $(get_pod_mau "https://we.loveprivacy.club")      |
-| [Yarn Pods](https://yarn.yarnpods.com)              | Australia/Perth    | [@screem](https://yarn.yarnpods.com/~screem/)          | $(get_pod_mau "https://yarn.yarnpods.com")        |
 | [zn80.net](https://yarn.zn80.net)                   | Europe/Germany     | [@carsten](https://yarn.zn80.net/~carsten/)            | $(get_pod_mau "https://yarn.zn80.net")            |
 | [neotxt.dk](https://neotxt.dk)                      | Europe/Scandinavia | [@darch](https://neotxt.dk/~darch/)                    | $(get_pod_mau "https://neotxt.dk")                |
 EOF
