01982a834c723ac2f54f99736794ecfae53ee9fa
James Mills <prologic@shortcircuit.net.au>
2022-08-06 11:41:11 +1000

    Remove MAU from Pods table

--- .zs/pods.sh
+++ .zs/pods.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-get_pod_mau() {
-  uri="$1"
-  mau="$(curl -qso - "$uri/metrics" | grep -E '^yarnd_server_mau \d+' | awk '{ print $2 }')"
-  printf "%s" "$mau"
-}
-
 cat <<EOF
-| **Pod**                                             | **Location**       | **Operator**                                           |                   **Users**                       |
-| :-------------------------------------------------- | ------------------ | ------------------------------------------------------ | ------------------------------------------------- |
-| [twtxt.net](https://twtxt.net)                      | Australia/Brisbane | [@prologic](https://twtxt.net/~prologic)               | $(get_pod_mau "https://twtxt.net")                |
-| [nfld](https://twt.nfld.uk)                         | United Kingdom     | [@jlj](https://twt.nfld.uk/~jlj)                       | $(get_pod_mau "https://twt.nfld.uk")              |
-| [We.Love.Privacy.Club](https://we.loveprivacy.club) | Australia          | [@eldersnake](https://we.loveprivacy.club/~eldersnake) | $(get_pod_mau "https://we.loveprivacy.club")      |
-| [zn80.net](https://yarn.zn80.net)                   | Europe/Germany     | [@carsten](https://yarn.zn80.net/~carsten/)            | $(get_pod_mau "https://yarn.zn80.net")            |
-| [neotxt.dk](https://neotxt.dk)                      | Europe/Scandinavia | [@darch](https://neotxt.dk/~darch/)                    | $(get_pod_mau "https://neotxt.dk")                |
+| **Pod**                                             | **Location**       | **Operator**                                           |
+| :-------------------------------------------------- | ------------------ | ------------------------------------------------------ |
+| [twtxt.net](https://twtxt.net)                      | Australia/Brisbane | [@prologic](https://twtxt.net/~prologic)               |
+| [nfld](https://twt.nfld.uk)                         | United Kingdom     | [@jlj](https://twt.nfld.uk/~jlj)                       |
+| [We.Love.Privacy.Club](https://we.loveprivacy.club) | Australia          | [@eldersnake](https://we.loveprivacy.club/~eldersnake) |
+| [zn80.net](https://yarn.zn80.net)                   | Europe/Germany     | [@carsten](https://yarn.zn80.net/~carsten/)            |
+| [neotxt.dk](https://neotxt.dk)                      | Europe/Scandinavia | [@darch](https://neotxt.dk/~darch/)                    |
 EOF
