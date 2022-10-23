c96cfb79ceb2d4cfe129e974be7424a2f1dcc3b3
Arya Kiran <aryak@vern.cc>
2022-10-23 20:19:13 +0530

    Add yn.vern.cc to podlist
    Signed-off-by: Arya Kiran <aryak@vern.cc>

--- .zs/pods.sh
+++ .zs/pods.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 cat <<EOF
-| **Pod**                                             | **Location**       | **Operator**                                                |
-|:----------------------------------------------------|--------------------|-------------------------------------------------------------|
-| [twtxt.net](https://twtxt.net)                      | Australia/Brisbane | [@prologic](https://twtxt.net/~prologic)                    |
-| [nfld](https://twt.nfld.uk)                         | United Kingdom     | [@jlj](https://twt.nfld.uk/~jlj)                            |
-| [We.Love.Privacy.Club](https://we.loveprivacy.club) | Australia          | [@eldersnake](https://we.loveprivacy.club/~eldersnake)      |
-| [zn80.net](https://yarn.zn80.net)                   | Europe/Germany     | [@carsten](https://yarn.zn80.net/~carsten/)                 |
-| [neotxt.dk](https://neotxt.dk)                      | Europe/Scandinavia | [@darch](https://neotxt.dk/~darch/)                         |
-| [.kyoko](https://social.kyoko-project.wer.ee)       | Europe/Luxembourg  | [@akoizumi](https://social.kyoko-project.wer.ee/~akoizumi/) |
+| **Pod**                                             | **Location**       | **Operator**                                                               |
+|:----------------------------------------------------|--------------------|----------------------------------------------------------------------------|
+| [twtxt.net](https://twtxt.net)                      | Australia/Brisbane | [@prologic](https://twtxt.net/~prologic)                                   |
+| [nfld](https://twt.nfld.uk)                         | United Kingdom     | [@jlj](https://twt.nfld.uk/~jlj)                                           |
+| [We.Love.Privacy.Club](https://we.loveprivacy.club) | Australia          | [@eldersnake](https://we.loveprivacy.club/~eldersnake)                     |
+| [zn80.net](https://yarn.zn80.net)                   | Europe/Germany     | [@carsten](https://yarn.zn80.net/~carsten/)                                |
+| [neotxt.dk](https://neotxt.dk)                      | Europe/Scandinavia | [@darch](https://neotxt.dk/~darch/)                                        |
+| [.kyoko](https://social.kyoko-project.wer.ee)       | Europe/Luxembourg  | [@akoizumi](https://social.kyoko-project.wer.ee/~akoizumi/)                |
+| [yn.vern.cc](https://yn.vern.cc)                    | America/Eastern    | [@aryak](https://yn.vern.cc/~aryak) and [@cobra](https://yn.vern.cc/~cobra)|
 EOF
